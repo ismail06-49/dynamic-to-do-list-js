885346a4cabbdc24fe0f710999c8c0ef5cc2241d
ismail haddad <151532205+ismail06-49@users.noreply.github.com>
2024-10-08 00:44:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Create a remove button for the task
             const removeButton = document.createElement('button');
             removeButton.textContent = "Remove";
-            removeButton.className = 'remove-btn'
+            removeButton.classList.add('remove-btn')
 
             // Define the behavior for when the remove button is clicked
             removeButton.onclick = function () {
